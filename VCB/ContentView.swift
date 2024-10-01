4cc5323659be8b3ef65d90709257e3a5ca77dd81
helinyu <helinyu@users.noreply.github.com>
2024-10-02 01:51:21 +0800

    init vcb project

---
+++ VCB/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  VCB
+//
+//  Created by helinyu on 2024/10/2.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ VCBTests/VCBTests.swift
@@ -0,0 +1,17 @@
+//
+//  VCBTests.swift
+//  VCBTests
+//
+//  Created by helinyu on 2024/10/2.
+//
+
+import Testing
+@testable import VCB
+
+struct VCBTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
